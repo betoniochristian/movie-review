57a8d1446b8a17a977e84b37840804082e4712b4
betoniochristian <betoniochristian22@gmail.com>
2024-09-18 10:57:26 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,21 @@
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import { Link } from 'react-router-dom';
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Nav from './Nav';
 import Home from './Home';
 import MovieDetails from './MovieDetails'; 
 import TvShowDetails from './TvShowDetails';
-import ListMovie from './ListMovie'
-import ListTv from './ListTv'
-import TopMovie from './TopMovie'
-import SearchResult from './SearchResult'
-import Action from './Action'
-import Drama from './Drama'
-import Horror from './Horror'
-import Adventure from './Adventure'
-import History from './History'
-import Science from './Science'
-import Thriller from './Thriller'
-import War from './War'
+import ListMovie from './ListMovie';
+import ListTv from './ListTv';
+import TopMovie from './TopMovie';
+import SearchResult from './SearchResult';
+import Action from './Action';
+import Drama from './Drama';
+import Horror from './Horror';
+import Adventure from './Adventure';
+import History from './History';
+import Science from './Science';
+import Thriller from './Thriller';
+import War from './War';
 import './App.css';
 
 function App() {
@@ -26,19 +25,20 @@ function App() {
   };
 
   const handleClick = () => {
-    if(isMenuOpen){
+    if (isMenuOpen) {
       toggleMenu();
     }
-  }
+  };
 
   return (
     <Router>
       <div className={`app ${isMenuOpen ? 'menu-open' : ''}`}>
-        <Nav onMenuToggle={toggleMenu} onLinkClick={handleClick}/>
+        <Nav onMenuToggle={toggleMenu} onLinkClick={handleClick} />
         <div className={`menu ${isMenuOpen ? 'menu-open' : ''}`}>
-          <button className="side-button" onClick={toggleMenu}><i className="bi bi-arrow-left">Close Menu</i></button>
+          <button className="side-button" onClick={toggleMenu}>
+            <i className="bi bi-arrow-left">Close Menu</i>
+          </button>
           <ul>
-            <Link className="a-hover" to="#"></Link>
             <Link className="a-hover" to="/" onClick={handleClick}>Home</Link>
             <Link className="a-hover" to="/movies" onClick={handleClick}>Movies</Link>
             <Link className="a-hover" to="/tv-shows" onClick={handleClick}>TV Shows</Link>
@@ -46,14 +46,14 @@ function App() {
           </ul>
           <p className="genre-real">Genre</p>
           <div className="genre-div">
-          <Link className="genre-a" to="/action">Action</Link>
-          <Link className="genre-a" to="/drama">Drama</Link>
-          <Link className="genre-a" to="/horror">Horror</Link>
-          <Link className="genre-a" to="/adventure">Adventure</Link>
-          <Link className="genre-a" to="/history">History</Link>
-          <Link className="genre-a" to="/science">Science Fiction</Link>
-          <Link className="genre-a" to="/thriller">Thriller</Link>
-          <Link className="genre-a" to="/war">War</Link>
+            <Link className="genre-a" to="/action">Action</Link>
+            <Link className="genre-a" to="/drama">Drama</Link>
+            <Link className="genre-a" to="/horror">Horror</Link>
+            <Link className="genre-a" to="/adventure">Adventure</Link>
+            <Link className="genre-a" to="/history">History</Link>
+            <Link className="genre-a" to="/science">Science Fiction</Link>
+            <Link className="genre-a" to="/thriller">Thriller</Link>
+            <Link className="genre-a" to="/war">War</Link>
           </div>  
         </div>
 
@@ -73,11 +73,11 @@ function App() {
             <Route path="/tv-shows" element={<ListTv />} />
             <Route path="/top-imdb" element={<TopMovie />} />
             <Route path="/movie/:id" element={<MovieDetails />} />
-            <Route path="/tv/:id" element={<TvShowDetails />} /> 
+            <Route path="/tv/:id" element={<TvShowDetails />} />
             <Route path="/search" element={<SearchResult />} />
+            <Route path="*" element={<Navigate to="/" />} /> {/* Redirects to Home for any unknown routes */}
           </Routes>
         </div>
-
       </div>
     </Router>
   );
