839a6d873b7ebc889f8cc295b6a62969da6518a3
betoniochristian <betoniochristian22@gmail.com>
2024-09-18 11:14:51 +0800

    new commit

--- src/App.js
+++ src/App.js
@@ -39,7 +39,7 @@ function App() {
             <i className="bi bi-arrow-left">Close Menu</i>
           </button>
           <ul>
-            <Link className="a-hover" to="/" onClick={handleClick}>Home</Link>
+            <Link className="a-hover" to="/home" onClick={handleClick}>Home</Link>
             <Link className="a-hover" to="/movies" onClick={handleClick}>Movies</Link>
             <Link className="a-hover" to="/tv-shows" onClick={handleClick}>TV Shows</Link>
             <Link className="a-hover" to="/top-imdb" onClick={handleClick}>Top IMDB</Link>
